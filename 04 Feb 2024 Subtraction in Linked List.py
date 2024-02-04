7ac7d9ed3d25edddfb15e3c8460d286ea6090f12
Abhinav Tekam <122251068+abhi9T@users.noreply.github.com>
2024-02-04 17:59:28 +0530

    04 Feb 2024 Subtraction in Linked List.py
    04 Feb 2024 Done

---
+++ 04 Feb 2024 Subtraction in Linked List.py
@@ -0,0 +1,72 @@
+#User function Template for python3
+
+class Solution:
+    def subLinkedList(self, l1, l2): 
+        # Code here
+        # return head of difference list
+        # reverse both linked lists
+        def make_number(head):
+            c=0
+            cur=head
+            while(cur):
+                c=c*10+cur.data
+                cur=cur.next
+            return c
+        
+        a1=make_number(l1)
+        a2=make_number(l2)
+        a3=Node(abs(a1-a2))
+        return a3
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# Node Class
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # creates a new node with given value and appends it at the end of the linked list
+    def insert(self, val):
+        if self.head is None:
+            self.head = Node(val)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(val)
+            self.tail = self.tail.next
+
+# prints the elements of linked list starting with head
+def printList(n):
+    while n:
+        print(n.data,end='')
+        n = n.next
+    print()
+
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        
+        n = int(input())
+        arr1 = ( int(x) for x in input() )
+        LL1 = LinkedList()
+        for i in arr1:
+            LL1.insert(i)
+        
+        m = int(input())
+        arr2 = ( int(x) for x in input() )
+        LL2 = LinkedList()
+        for i in arr2:
+            LL2.insert(i)
+        
+        ob = Solution()
+        res = ob.subLinkedList(LL1.head, LL2.head)
+        printList(res)
+# } Driver Code Ends
