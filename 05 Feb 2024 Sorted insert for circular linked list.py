05cfde892946c4fbd085fd699fcdcd3c1924bda6
Abhinav Tekam <122251068+abhi9T@users.noreply.github.com>
2024-02-05 06:20:51 +0530

    05 Feb 2024 Sorted insert for circular linked list.py
    05 Feb 2024 Done

---
+++ 05 Feb 2024 Sorted insert for circular linked list.py
@@ -0,0 +1,97 @@
+#User function Template for python3
+
+'''
+class Node: 
+    # Constructor to initialize the node object 
+    def __init__(self, data): 
+        self.data = data 
+        self.next = None
+  '''
+class Solution:
+    def sortedInsert(self, head, data):
+        #code here
+        new_node = Node(data)
+        if not head:
+            new_node = Node(data)
+            new_node.next = new_node
+            return new_node
+        if new_node.data < head.data:
+            curr = head
+            while curr.next != head:
+                curr = curr.next
+            curr.next = new_node
+            new_node.next = head
+            return new_node
+        else:
+            curr = head
+            while curr.next != head:
+                if curr.next.data > new_node.data:
+                    break
+                curr = curr.next
+            temp = curr.next
+            curr.next = new_node
+            new_node.next = temp
+            return head
+
+
+#{ 
+ # Driver Code Starts
+#contributed by RavinderSinghPB
+class Node: 
+    # Constructor to initialize the node object 
+    def __init__(self, data): 
+        self.data = data 
+        self.next = None
+  
+class LinkedList: 
+  
+    # Function to initialize head 
+    def __init__(self): 
+        self.head = None
+        self.last=None
+  
+    # Function to insert a new node  
+    def push(self, data):
+        if not self.head:
+            nn=Node(data)
+            self.head=nn
+            self.last=nn
+            nn.next=nn
+            return
+        nn=Node(data)
+        nn.next=self.head
+        self.last.next=nn
+        self.last=nn 
+  
+
+# Utility function to print the linked LinkedList
+
+def printList(head):
+    if not head:
+        return
+    temp = head 
+    print (temp.data,end=' ') 
+    temp = temp.next
+    while(temp != head): 
+        print (temp.data,end=' ') 
+        temp = temp.next
+  
+    
+if __name__ =='__main__':
+    t=int(input())
+    for tcs in range(t):
+        n=int(input())
+        arr=[int(x) for x in input().split()]
+        data=int(input())
+
+        cll=LinkedList()
+        for e in arr:
+            cll.push(e)
+            
+        reshead=Solution().sortedInsert(cll.head,data)
+        printList(reshead)
+        print()
+        
+
+
+# } Driver Code Ends
